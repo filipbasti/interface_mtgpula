74066063cac59069e07d69338bde8b93a3a8f732
filipbasti <filipbastijanic@gmail.com>
2024-12-26 22:57:49 +0100

    added a button and list for current tournaments

---
+++ temp_mtgpula/src/components/PlayerList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <h1 class="my-4">Game Dashboard</h1>
+        <ul class="list-group">
+            <li v-for="player in players" :key="player.id" class="list-group-item">
+                {{ player.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlayerList',
+    props: {
+        players: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    font-family: Arial, sans-serif;
+}
+</style>
+
--- temp_mtgpula/src/views/CurrenTourView.vue
+++ temp_mtgpula/src/views/CurrenTourView.vue
@@ -1,17 +1,43 @@
 <template>
-<div>
-    <h3>CURRENT Tournament</h3>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-6">
+            <h3 class="text-center">CURRENT Tournament</h3>
+            <div class="card mt-4">
+                <div class="card-header">
+                    <h4>Users applied:</h4>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li v-for="player in activeUsers" :key="player.id" class="list-group-item d-flex justify-content-between align-items-center">
+                      {{ player.name }}
+                      <button @click="addUserToPlayersList(player)" class="btn btn-sm btn-outline-secondary">Add</button>
+                    </li>
+                  
+                </ul>
+            </div>
+           
+        </div>
+        <div class="col-md-6">
+            <PlayerList :players="activeUsers" />
+            <button class="btn btn-primary mt-4" @click="startTournament">Start Tournament</button>
+        </div>
+    </div>
 </div>
-
 </template>
+
 <script>    
 import socket from '../warehouse/socket';
+import PlayerList from '../components/PlayerList.vue';
 
 export default {
+    components: {
+        PlayerList
+    },
     data(){
        return{
         channel: "",
-        join_code: this.$route.params.join_code
+        join_code: this.$route.params.join_code,
+        activeUsers: []
     } 
     },
     mounted(){
@@ -28,16 +54,54 @@ export default {
         .receive("error", (resp) => {
           console.log("Unable to join", resp)
         })
-                // Push the "list_present_users" event to the server
-        this.channel.push("list_present_users", {})
-        .receive("ok", (resp) => {
-            console.log("Present users:", resp.players);
+        // Listen for user presence updates
+        this.channel.on("presence_state", state => {
+            this.handlePresenceState(state)
         })
-        .receive("error", (resp) => {
-            console.error("Failed to list present users:", resp);
-        });
-
-            }
-
+        this.channel.on("presence_diff", diff => {
+            this.handlePresenceDiff(diff)
+        })
+    },
+    methods: {
+        addUserToPlayersList(player) {
+            this.channel.push("add_player", {user_id: player.id, deck: "undef"})
+            .receive("ok", (resp) => {
+                console.log("Player added:", resp);
+            })
+            .receive("error", (resp) => {
+                console.error("Failed to add player:", resp);
+            });
+        },
+        startTournament() {
+            this.channel.push("start_tournament", {})
+            .receive("ok", (resp) => {
+                console.log("Tournament started:", resp);
+            })
+            .receive("error", (resp) => {
+                console.error("Failed to start tournament:", resp);
+            });
+        },
+        handlePresenceState(state) {
+            // Convert presence state to array of users
+            console.log(state)
+            this.activeUsers = Object.entries(state).map(([key, presence]) => ({
+                id: key,
+                name: presence.metas[0].user_name
+            }))
+        },
+        handlePresenceDiff(diff) {
+            // Handle users joining
+            diff.joins && Object.entries(diff.joins).forEach(([key, presence]) => {
+                this.activeUsers.push({
+                    id: key,
+                    name: presence.metas[0].user_name
+                })
+            })
+            // Handle users leaving
+            diff.leaves && Object.keys(diff.leaves).forEach(key => {
+                this.activeUsers = this.activeUsers.filter(user => user.id !== key)
+            })
+        }
+    }
 }
 </script>
